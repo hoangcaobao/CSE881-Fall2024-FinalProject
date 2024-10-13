bc768af19c6c5403f8c871e4c97dabfcb4227596
hoangbao <hoangbao@msu.edu>
2024-10-12 22:19:23 -0400

    update app

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 import streamlit as st
 from PIL import Image
 import random 
+import cv2
+import torch
+import torchvision.transforms as transforms
+import numpy as np
 
 st.title("Road Sign Detection Project")
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+label_mapping = {0: 'Cross Walk', 1: 'No Entry', 2: 'Pedestrian Crossing', 3: 'Speed Limit', 4: 'Stop', 5: 'Traffic Light', 6: 'Yield'}
 
 if uploaded_image is not None:
-    image = Image.open(uploaded_image)    
+    # Open the uploaded image
+    image = Image.open(uploaded_image)
+
+    # Resize
+    image_np = np.array(image)
+    image_resized = cv2.resize(image_np, (256, 256), interpolation=cv2.INTER_CUBIC)
+    image = Image.fromarray(image_resized)
+
     st.image(image, caption="Uploaded Image", use_column_width=True)
     
-    # We will add model output's later
-    label = random.choice(range(5))
+    # Load the model
+    model = torch.load("weights/VGG.pt", map_location=torch.device("cpu"))
+    model.eval() 
+    
+    # Transform to Tensor
+    image_prep = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    
+    image_tensor = image_prep(image).unsqueeze(0)  
+    
+    with torch.no_grad():  
+        output = model(image_tensor)
+        _, pred = torch.max(output.data, 1)
+    
+    # Get label from prediction
+    label = label_mapping[pred.item()]
     st.write(f"Label: {label}")
