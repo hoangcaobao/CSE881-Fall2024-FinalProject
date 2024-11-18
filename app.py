46cb21bb31ad2fdee27f2c194b97e3234763ada2
Bao Hoang <62338248+hoangcaobao@users.noreply.github.com>
2024-11-18 15:33:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import random 
-import cv2
 import torch
 import torchvision.transforms as transforms
 import numpy as np
@@ -31,6 +30,15 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+    ### About the Project
+    With the rapid progress in autonomous driving technology, detecting and classifying road signs has become a critical task. Road signs provide essential information for safe and efficient navigation, making their accurate detection indispensable for modern autonomous vehicles.
+
+    This project leverages cutting-edge **Computer Vision** and **Deep Learning** techniques to build and evaluate high-performance road sign detection models. The models are trained on diverse road sign images collected from Google Images, Google Shopping, and Kaggle, covering 4 categories **Stop**, **Speed Limit**, **Traffic Light**, and **Cross Walk**.
+
+    You can upload an image of a road sign below to see how well our fine-tuned models (ResNet and VGG) can classify it!
+""")
+
 option = st.selectbox(
     "Which Computer Vision Architectures you want to use?",
     ("VGG", "ResNet"),
@@ -45,8 +53,8 @@ if uploaded_image is not None:
 
     # Resize
     image_np = np.array(image)
-    image_resized = cv2.resize(image_np, (256, 256), interpolation=cv2.INTER_CUBIC)
-    image = Image.fromarray(image_resized)
+    image = Image.fromarray(image_np)
+    image = image.resize((256, 256))
 
     if image.mode == 'RGBA':
         image = image.convert('RGB')
